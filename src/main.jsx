f3f83ef92a42a1d1cb7f688a6946750dc9ebc35f
Johnson-k20 <65182977+Johnson-k20@users.noreply.github.com>
2026-07-28 11:45:19 +0300

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
-import "./asset
-<assets />
-<assets />
-<css />index.css";
+import "./assets/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
